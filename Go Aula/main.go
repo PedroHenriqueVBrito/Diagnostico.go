253b20a8bf266159fb8709ea28c02e189299a040
PedroHenriqueVBrito <henriquebritopb@gmail.com>
2026-03-15 23:12:49 -0300

    Add files via upload

--- Go Aula/main.go
+++ Go Aula/main.go
@@ -1,17 +1,31 @@
-/*
-Falha A: Escalabilidade; O servidor do site estava excedendo seu limite e consequentemente estava se tornando mais lento e por está causa inesperada efetuou na perda de usuários.
-Falha B: Confiabilidade; Em sites de compra os clientes quando compram algo lá esperam que nada seu seja exposto para outros usuários e por causa de grandes picos de acessos isso 
-se tornou possível então a perda de usuários é constante por não sentirem que sua confiabilidade do site está segura.
-Falha C: Manutenabilidade; O sistema que foi feito de uma maneira extremamaente mas mal explicado e uma equipe de pessoas que podem se expecializar em várias áreas diferentes podem 
-acabar não conseguindo completar a correção do bug completamente.
-*/
-
 package main
+
 import "fmt"
 
-func main(){
-	fmt.Println("Ambiente configurado por: Profº. Marcos André")
- 
-	fmt.Println("Partiu Carnaval, mas com o mindset de Alta Performance!")
+func deposito(saldo *float64, valor float64) {
+	*saldo += valor
+}
 
+func saque(saldo *float64, valor float64) {
+	if valor > *saldo {
+		fmt.Println("Saldo insuficiente")
+		return
+	}
+	*saldo -= valor
+}
+
+func main() {
+
+	saldo := 1000.0
+
+	fmt.Printf("Saldo inicial: %.2f\n", saldo)
+
+	deposito(&saldo, 200)
+	fmt.Printf("Após depósito: %.2f\n", saldo)
+
+	saque(&saldo, 150)
+	fmt.Printf("Após saque: %.2f\n", saldo)
+
+	saque(&saldo, 1200)
+	fmt.Printf("Saldo da conta após saque insuficiente: %.2f\n", saldo)
 }
